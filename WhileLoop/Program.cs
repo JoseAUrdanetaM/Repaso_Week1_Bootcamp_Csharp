bd2ebb3ed2ad8bdb1329cdc1cc4b3fcfeb6c40ca
Jose <urdaneta.j@outlook.com>
2025-04-04 22:04:41 -0500

    Nuevos cambios

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a los while loops");
+
+            int contadorTabla = 0;
+            int valorTabla;
+            int resutado;
+            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            valorTabla = Convert.ToInt32(Console.ReadLine());
+            while(contadorTabla <= 12)
+            {
+                resutado = valorTabla * contadorTabla;
+                Console.WriteLine("La tabla del número: " + valorTabla + " es igual a: " + valorTabla + "x" + contadorTabla + "=" + resutado);
+                contadorTabla++;
+            }
+         
+        }
+    }
+}
