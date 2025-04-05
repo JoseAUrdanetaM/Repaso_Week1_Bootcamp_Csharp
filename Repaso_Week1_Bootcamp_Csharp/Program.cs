f98e4c70c6be4ebb43674c20e761e026b5b738cc
Jose <urdaneta.j@outlook.com>
2025-04-04 20:48:15 -0500

    Adding comments

--- Repaso_Week1_Bootcamp_Csharp/Program.cs
+++ Repaso_Week1_Bootcamp_Csharp/Program.cs
@@ -4,12 +4,33 @@
     {
         static void Main(string[] args)
         {
+
+            int primerNumero;
+            int segundoNumero;
+            int tercerNumero;
+
             Console.WriteLine("");
             Console.WriteLine("============================");
-            Console.WriteLine("Ingresa el valor a comprobar: ");
+            Console.WriteLine("¿Cuál es el número mayor de 3 números: ");
             Console.WriteLine("============================");
 
-            
+            primerNumero = Convert.ToInt32(Console.ReadLine());
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+            tercerNumero = Convert.ToInt32(Console.ReadLine());
+
+            //Obtener el área del circulo 
+            Console.WriteLine("");
+            Console.WriteLine("===============  ============");
+            Console.WriteLine("¿Cuál es el número mayor de 3 números: ");
+            Console.WriteLine("============================");
+
+            //Comprobar si has escogido circulo o cuadrado
+            //Si es circulo necesito que pidas el radio
+            //Si has elegido rectangulo, tendría que pedir largo y ancho
+            //Si has escodigo una opción que no es circulo o rectangulo, un mensaje de error
+
+
+
         }
     }
 }
