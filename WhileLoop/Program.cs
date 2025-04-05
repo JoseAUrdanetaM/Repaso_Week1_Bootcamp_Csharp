bdf8b2000a84e2a633d1322a29945253e0c1678b
Jose <urdaneta.j@outlook.com>
2025-04-04 22:38:03 -0500

    Ejercicio bucles

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -4,20 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenido a los while loops");
+            //Console.WriteLine("Bienvenido a los while loops");
 
-            int contadorTabla = 0;
-            int valorTabla;
-            int resutado;
-            Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
-            valorTabla = Convert.ToInt32(Console.ReadLine());
-            while(contadorTabla <= 12)
+            //int contadorTabla = 0;
+            //int valorTabla;
+            //int resutado;
+            //Console.WriteLine("Ingrese el valor de la tabla de multiplicar");
+            //valorTabla = Convert.ToInt32(Console.ReadLine());
+            //while(contadorTabla <= 12)
+            //{
+            //    resutado = valorTabla * contadorTabla;
+            //    Console.WriteLine("La tabla del número: " + valorTabla + " es igual a: " + valorTabla + "x" + contadorTabla + "=" + resutado);
+            //    contadorTabla++;
+            //}
+
+
+
+            // Bucle estrella
+            int inicio = 1;
+            while (inicio <= 5)
             {
-                resutado = valorTabla * contadorTabla;
-                Console.WriteLine("La tabla del número: " + valorTabla + " es igual a: " + valorTabla + "x" + contadorTabla + "=" + resutado);
-                contadorTabla++;
+                int b = 1;
+
+                while (b <= inicio)
+                {
+                    Console.Write(b + " ");
+                    b++;
+                }
+                Console.Write( "*");
+
+                Console.WriteLine();
+                inicio++;
             }
-         
+
         }
     }
 }
