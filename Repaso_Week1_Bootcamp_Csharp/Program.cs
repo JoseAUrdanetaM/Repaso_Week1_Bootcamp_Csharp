3b1b75ffcf844402edc1ddc22c71550bb0816b81
Jose <urdaneta.j@outlook.com>
2025-04-04 20:21:34 -0500

    Add project files.

---
+++ Repaso_Week1_Bootcamp_Csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Repaso_Week1_Bootcamp_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("============================");
+            Console.WriteLine("Ingresa el valor a comprobar: ");
+            Console.WriteLine("============================");
+
+            
+        }
+    }
+}
