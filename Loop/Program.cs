d4ac04c53b5997050ad18740526786351a974300
Jose <urdaneta.j@outlook.com>
2025-04-09 21:33:30 -0500

    Ejercicio Loop

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -1,24 +1,9 @@
-﻿namespace ForLoop
+﻿namespace Loop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Ejercicio #1 Tabla de Multiplicar 
-
-            //Console.WriteLine("Bienvenido a la tabla de multiplicación, ingrese el número de tabla a multiplicar:");
-            //int baseNumber = Convert.ToInt32(Console.ReadLine());
-            //int result;
-            //Console.WriteLine("");
-            //Console.WriteLine($"Tabla del {baseNumber}");
-            //for (int i = 0; i < 11; i++)
-            //{
-            //    result = baseNumber * i;
-            //    Console.WriteLine($"{baseNumber} x {i} = {result}");
-            //}
-
-            //Ejercicio #2 Patrones en escalera 
-
             Console.WriteLine("Generador de patrón escalera");
 
             Console.WriteLine("Inserte la cantidad de pisos");
@@ -36,7 +21,6 @@
                 Console.WriteLine("");
             }
             Console.WriteLine("Se acabó el bucle");
-
         }
     }
 }
--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace WhileLoop
+﻿    namespace WhileLoop
 {
     internal class Program
     {
