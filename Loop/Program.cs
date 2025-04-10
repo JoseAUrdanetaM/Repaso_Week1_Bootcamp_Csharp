e8747ddb5d2df53db01759dc690c0c4beb390231
Jose <urdaneta.j@outlook.com>
2025-04-09 21:58:35 -0500

    Ejercicio Loop piramide

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -11,15 +11,43 @@
 
             Console.WriteLine("");
 
-            for (int i = 0; i <= baseTriangleNumber; i++)
+            //for (int i = 0; i <= baseTriangleNumber; i++)
+            //{
+            //    Console.Write("* ");
+            //    for (int b = 1; b <= i; b++)
+            //    {
+            //        Console.Write(b + " ");
+            //    }
+            //    Console.WriteLine(""); 
+            //}
+
+            /*
+             * 
+             * -----*
+             * ----* *
+             * ---* * *
+             * --* * * *
+             * -* * * * *
+             * 
+             * 
+             */
+
+            for (int i = 1; i <= baseTriangleNumber; i++)
             {
-                Console.Write("* ");
-                for (int b = 1; b <= i; b++)
+             
+                for (int b = baseTriangleNumber; b >= i; b--)
                 {
-                    Console.Write(b + " ");
+                    Console.Write("- ");
                 }
-                Console.WriteLine(""); 
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine("");
             }
+
             Console.WriteLine("Se acabó el bucle");
         }
     }
