4c680cd05f42bd327cd3c0657f7fe8904373bb20
Jose <urdaneta.j@outlook.com>
2025-04-13 20:37:47 -0500

    Actualización de Retos, Semana 1 y 2

--- Semana 1/Week1_Ejercicios/Program.cs
+++ Semana 1/Week1_Ejercicios/Program.cs
@@ -2,9 +2,56 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+             static void Main(string[] args)
+            {
+
+                    Console.WriteLine("Calcualdora .Net");
+                    Console.WriteLine("----------------");
+
+                    Console.Write("Ingresa el primer número: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Ingresa el segundo número: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                    Console.Write("¿Qué operación deseas realizar? (+, -, *, /): ");
+                    string operacion = Console.ReadLine();
+
+                    double resultado = 0;
+
+                    if (operacion == "+")
+                    {
+                        resultado = num1 + num2;
+                        Console.WriteLine($"El resultado {num1} + {num2} es:{resultado}");
+                    }
+                    else if (operacion == "-")
+                    {
+                        resultado = num1 - num2;
+                        Console.WriteLine($"El resultado {num1} - {num2} es: {resultado}");
+                    }
+                    else if (operacion == "*")
+                    {
+                        resultado = num1 * num2;
+                        Console.WriteLine($"El resultado {num1} * {num2} es: {resultado}");
+                    }
+                    else if (operacion == "/")
+                    {
+                        if (num2 > 0)
+                        {
+                            resultado = num1 + num2;
+                            Console.WriteLine($"El resultado {num1} / {num2} es: {resultado}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error matemático, no se puede dividir entre 0");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El operador ingresado es inválido");
+                    }
+                }
+            }
     }
 }
--- Week2_Ejercicios/Program.cs
+++ Week2_Ejercicios/Program.cs
@@ -4,170 +4,217 @@
     {
         static void Main(string[] args)
         {
-            //RETO #1: Arrays Unilaterales
 
-            //SISTEMA DE CALIFICACIONES
+            //RETO BUCLES #1 Tabla de Multiplicar ##################################################################
+            //Console.WriteLine("Bienvenido a la tabla de multiplicación, ingrese el número de tabla a multiplicar:");
+            //int baseNumber = Convert.ToInt32(Console.ReadLine());
+            //int result;
+            //Console.WriteLine("");
+            //Console.WriteLine($"Tabla del {baseNumber}");
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    result = baseNumber * i;
+            //    Console.WriteLine($"{baseNumber} x {i} = {result}");
+            //}
+            //######################################################################################################
 
-            // Objetivo: Desarrollar un programa que gestione las calificaciones de estudiantes y realice
-            // diferentes operaciones estadísticas.
+            //RETO BUCLES #2 Patrones en escalera ##################################################################
+            //Console.WriteLine("");
+            //Console.WriteLine("Generador de patrón escalera");
+            //Console.WriteLine("Inserte la cantidad de pisos");
+            //int baseTriangleNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("");
+            //for (int i = 0; i <= baseTriangleNumber; i++)
+            //{
+            //    Console.Write("* ");
+            //    for (int b = 1; b <= i; b++)
+            //    {
+            //        Console.Write(b + " ");
+            //    }
+            //    Console.WriteLine("");
+            //}
+            //Console.WriteLine("Se acabó el bucle");
+            //######################################################################################################
 
-            //    Requisitos:
-            //    1.Crear un array para almacenar las calificaciones de 10 estudiantes(valores entre 0 y 100)
-            //    2.Implementar las siguientes funcionalidades:
-            //    -Calcular el promedio de calificaciones
-            //       -Encontrar la calificación más alta y la más baja
-            //       -Calcular cuántos estudiantes aprobaron(calificación >= 60)
-            //       - Mostrar las calificaciones en orden ascendente
-            //       - Mostrar cuántos estudiantes obtuvieron calificaciones en los siguientes rangos:
-            //         *0 - 59(Reprobado)
-            //         * 60 - 69(Suficiente)
-            //         * 70 - 79(Bien)
-            //         * 80 - 89(Notable)
-            //         * 90 - 100(Excelente)
+            //RETO BUCLES #3 Write a program in C# Sharp to display the multiplication table of a given integer. ####
+            // Test Data:
+            // Input the number(Table to be calculated) : 15
+            // Expected Output :
+            // 15 X 1 = 15...
+            // ...
+            // 15 X 10 = 150
 
-            //    Extra(opcional):
-            //    -Permitir que el usuario ingrese las calificaciones
-            //    -Implementar un menú para que el usuario elija la operación a realizar
-            //    */
-
-            Console.WriteLine("SISTEMA DE CALIFICACIONES");
-            Console.WriteLine("=========================\n");
-
-            // 1. Declarar el array de calificaciones
-            double[] calificaciones = new double[10];
-            string bienvenida;
-            // 2. Ingresar las calificaciones (puede ser manual o automático)
-            IngresarCalificaciones(calificaciones);
-
-            //// // 3. Mostrar todas las calificaciones
-            MostrarCalificaciones(calificaciones);
-
-            //// // 4. Calcular y mostrar estadísticas
-            CalcularEstadisticas(calificaciones);
-
-            //// // 5. Ordenar y mostrar calificaciones
-            //OrdenarCalificaciones(calificaciones);
-
-            //// // 6. Mostrar distribución por rangos
-            //MostrarDistribucionRangos(calificaciones);
-
-
-        }
-
-        private static void MostrarDistribucionRangos(double[] calificaciones)
-        {
-
-
-        }
-
-        private static void OrdenarCalificaciones(double[] calificaciones)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void CalcularEstadisticas(double[] calificaciones)
-        {
-            double promedio = 0;
-            double resultado;
-            for (int i = 0; i < calificaciones.Length; i++)
-            {
-                promedio += calificaciones[i];
-            }
-            resultado = promedio / calificaciones.Length;
-
-            Console.WriteLine($"El promedio de notas es {resultado}");
-        }
-
-        private static void MostrarCalificaciones(double[] calificaciones)
-        {
-            Console.WriteLine("Las notas son las siguientes:");
-
-            double numeroMenor = calificaciones[0];
-            double numeroMayor = calificaciones[0];
-
-            for (int i = 0; i < calificaciones.Length; i++)
-            {
+            //Console.WriteLine("\nBienvenido a los while loops");
+            //Console.WriteLine("Ingrese el valor de la tabla de multiplicar:");
+            //int valorTabla = Convert.ToInt32(Console.ReadLine());
+            //int contadorTabla = 0;
+            //while (contadorTabla <= 12)
+            //{
+            //    int resultado = valorTabla * contadorTabla;
+            //    Console.WriteLine($"{valorTabla} x {contadorTabla} = {resultado}");
+            //    contadorTabla++;
+            //}
+            //######################################################################################################
 
 
-                if (numeroMenor > calificaciones[i])
-                {
-                    numeroMenor = calificaciones[i];
-                }
-                else if (numeroMayor < calificaciones[i])
-                {
-                    numeroMayor = calificaciones[i];
-                }
+            ////  RETO BUCLES #4 Patrones en escalera ##############################################################
+            //int inicio = 1;
+            //while (inicio <= 5)
+            //{
+            //    int b = 1;
+            //    while (b <= inicio)
+            //    {
+            //        Console.Write(b + " ");
+            //        b++;
+            //    }
+            //    Console.Write("*");
+            //    Console.WriteLine();
+            //    inicio++;
+            //}
+            //######################################################################################################
 
-            }
-            Console.WriteLine($"El La nota Mayor es {numeroMayor}");
-            Console.WriteLine($"El La nota Menor es {numeroMenor}");
+
+            //RETO ARRAYS #1: Arrays Unilaterales SISTEMA DE CALIFICACIONES ########################################
+            /*
+             Objetivo: Desarrollar un programa que gestione las calificaciones de estudiantes y realice
+             diferentes operaciones estadísticas.
+
+                Requisitos:
+                1.Crear un array para almacenar las calificaciones de 10 estudiantes(valores entre 0 y 100)
+                2.Implementar las siguientes funcionalidades:
+                -Calcular el promedio de calificaciones
+                   -Encontrar la calificación más alta y la más baja
+                   -Calcular cuántos estudiantes aprobaron(calificación >= 60)
+                   - Mostrar las calificaciones en orden ascendente
+                   - Mostrar cuántos estudiantes obtuvieron calificaciones en los siguientes rangos:
+                     *0 - 59(Reprobado)
+                     * 60 - 69(Suficiente)
+                     * 70 - 79(Bien)
+                     * 80 - 89(Notable)
+                     * 90 - 100(Excelente)
+
+                Extra(opcional):
+                -Permitir que el usuario ingrese las calificaciones
+                -Implementar un menú para que el usuario elija la operación a realizar
+            */
+
+        //    {
+        //        Console.WriteLine("SISTEMA DE CALIFICACIONES");
+        //        Console.WriteLine("=========================\n");
+
+        //        double[] calificaciones = new double[10];
+
+        //        IngresarCalificaciones(calificaciones);
+        //        MostrarCalificaciones(calificaciones);
+        //        CalcularEstadisticas(calificaciones);
+        //        OrdenarCalificaciones(calificaciones);
+        //        MostrarDistribucionRangos(calificaciones);
+        //    }
+
+        //private static void IngresarCalificaciones(double[] calificaciones)
+        //{
+        //    for (int i = 0; i < calificaciones.Length; i++)
+        //    {
+        //        double entrada;
+        //        do
+        //        {
+        //            Console.Write($"Ingrese la nota #{i + 1} (0 - 100): ");
+        //            entrada = Convert.ToDouble(Console.ReadLine());
+
+        //            if (entrada < 0 || entrada > 100)
+        //            {
+        //                Console.WriteLine("Número inválido. Intenta de nuevo.");
+        //            }
+
+        //        } while (entrada < 0 || entrada > 100);
+
+        //        calificaciones[i] = entrada;
+        //    }
+        //}
+
+        //private static void MostrarCalificaciones(double[] calificaciones)
+        //{
+        //    Console.WriteLine("\nListado de calificaciones:");
+        //    foreach (var nota in calificaciones)
+        //    {
+        //        Console.Write(nota + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //    double max = calificaciones[0];
+        //    double min = calificaciones[0];
+
+        //    foreach (var nota in calificaciones)
+        //    {
+        //        if (nota > max) max = nota;
+        //        if (nota < min) min = nota;
+        //    }
+
+        //    Console.WriteLine($"Nota más alta: {max}");
+        //    Console.WriteLine($"Nota más baja: {min}");
+        //}
+
+        //private static void CalcularEstadisticas(double[] calificaciones)
+        //{
+        //    double suma = 0;
+        //    int aprobados = 0;
+
+        //    foreach (var nota in calificaciones)
+        //    {
+        //        suma += nota;
+        //        if (nota >= 60)
+        //            aprobados++;
+        //    }
+
+        //    double promedio = suma / calificaciones.Length;
+
+        //    Console.WriteLine($"\nPromedio general: {promedio:F2}");
+        //    Console.WriteLine($"Cantidad de estudiantes aprobados (>=60): {aprobados}");
+        //}
+
+        //private static void OrdenarCalificaciones(double[] calificaciones)
+        //{
+        //    Array.Sort(calificaciones);
+        //    Console.WriteLine("\nCalificaciones en orden ascendente:");
+        //    foreach (var nota in calificaciones)
+        //    {
+        //        Console.Write(nota + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        //private static void MostrarDistribucionRangos(double[] calificaciones)
+        //{
+        //    int reprobado = 0, suficiente = 0, bien = 0, notable = 0, excelente = 0;
+
+        //    foreach (var nota in calificaciones)
+        //    {
+        //        if (nota < 60) reprobado++;
+        //        else if (nota < 70) suficiente++;
+        //        else if (nota < 80) bien++;
+        //        else if (nota < 90) notable++;
+        //        else excelente++;
+        //    }
+
+        //    Console.WriteLine("\nDistribución de calificaciones:");
+        //    Console.WriteLine($"0 - 59 (Reprobado): {reprobado}");
+        //    Console.WriteLine($"60 - 69 (Suficiente): {suficiente}");
+        //    Console.WriteLine($"70 - 79 (Bien): {bien}");
+        //    Console.WriteLine($"80 - 89 (Notable): {notable}");
+        //    Console.WriteLine($"90 - 100 (Excelente): {excelente}");
+        //}
 
 
-        }
 
+        //RETO ARRAYS #2: List
+        //Operaciones del reto
 
-        private static void IngresarCalificaciones(double[] calificaciones)
-        {
-            for (int i = 0; i < calificaciones.Length; i++)
-            {
-                double entrada = calificaciones[0];
-                Console.WriteLine($"Ingrese la nota #{i + 1}");
-                entrada = Convert.ToInt32(Console.ReadLine());
-
-                if (entrada >= 0 && entrada <= 100)
-                {
-                    calificaciones[i] = entrada;
-                }
-                else
-                {
-                    Console.WriteLine("Numero ingresado inválido");
-                }
-            }
-        }
-
-        /*
-         * 
-         * 
-        RETO #2: List
-        // Operaciones del reto
         // Crear la lista, agregar los valores y hacer las operaciones
-        // Filtrar los libros con stock de mas de 10 unidades
-        // Filtrar el stock por precio mas grande de 10.99 soles
-        // Encontrar el indice del libro que empiece con "El"
-
-
-        RETO #3 OPCIONAL: MATRIZ DE TEMPERATURAS SEMANALES
-
-           Objetivo: Crear un programa que registre y analice temperaturas diarias por semana usando arrays bidimensionales.
-
-           Requisitos:
-           1. Crear una matriz para almacenar las temperaturas de 4 semanas (filas) y 7 días por semana (columnas)
-           2. Permitir al usuario introducir las temperaturas o generarlas aleatoriamente entre 10°C y 35°C
-           3. Calcular y mostrar:
-              - La temperatura media de cada semana
-              - La temperatura media de cada día de la semana (todos los lunes, todos los martes, etc.)
-              - El día más caluroso y el más frío (indicando semana y día)
-           4. Mostrar gráficamente la matriz de temperaturas con formato adecuado
-
-           Bonus (opcional):
-           - Generar un gráfico ASCII simple que muestre la evolución de temperaturas
-           - Permitir buscar días con temperatura superior o inferior a un valor introducido por el usuario
-           */
-
-        
-            // Operaciones del reto
-            // Crear la lista, agregar los valores y hacer las operaciones
-            // Filtrar los libros con stock de mas de 10 unidades
-            // Filtrar el stock por precio mas grande de 10.99 soles
-            // Encontrar el indice del libro que empiece con "El"
-        }
-
-
         List<Libro> libros = new List<Libro>
-        {
+            {
                 new Libro { Nombre = "Cien años de soledad", Precio = 34.50, Stock = 12 },
-                new Libro { Nombre = "1984", Precio = 19.99, Stock = 8 },
                 new Libro { Nombre = "Don Quijote de la Mancha", Precio = 42.75, Stock = 4 },
+                new Libro { Nombre = "1984", Precio = 19.99, Stock = 8 },
                 new Libro { Nombre = "El nombre del viento", Precio = 29.90, Stock = 10 },
                 new Libro { Nombre = "La sombra del viento", Precio = 27.40, Stock = 7 },
                 new Libro { Nombre = "Crónica de una muerte anunciada", Precio = 15.60, Stock = 15 },
@@ -175,52 +222,42 @@
                 new Libro { Nombre = "Rayuela", Precio = 31.99, Stock = 3 },
                 new Libro { Nombre = "Fahrenheit 451", Precio = 18.25, Stock = 9 },
                 new Libro { Nombre = "El alquimista", Precio = 20.00, Stock = 6 },
-        };
-
-            // Operaciones del reto
-            // Crear la lista, agregar los valores y hacer las operaciones
-
+             };
 
             // Filtrar los libros con stock de mas de 10 unidades
-//            List<Libro> stockSobreDiez = libros.FindAll(p => p.Stock > 10);
-//            Console.WriteLine($"Libros con más de 10 unidades:");
-//            foreach(var stock in stockSobreDiez)
-//            {
-//                Console.WriteLine($"{stock.Nombre}, {stock.Stock}");
-//            }
+            List<Libro> stockSobreDiez = libros.FindAll(p => p.Stock > 10);
+            Console.WriteLine($"Libros con más de 10 unidades:");
+            foreach (var stock in stockSobreDiez)
+            {
+                Console.WriteLine($"{stock.Nombre}, {stock.Stock}");
+            }
+            Console.WriteLine("");
 
-//            Console.WriteLine("");
+            // Filtrar el stock por precio mas grande de 10.99 soles
+            List<Libro> precios = libros.FindAll(p => p.Precio > 10.99);
+            Console.WriteLine($"Libros mayores a S/10.99:");
+            foreach (var precio in precios)
+            {
+            Console.WriteLine($"{precio.Nombre}, S/{precio.Precio}");
+            }
+            Console.WriteLine("");
 
-//            // Filtrar el stock por precio mas grande de 10.99 soles
-//            List<Libro> precios = libros.FindAll(p => p.Precio > 10.99);
-//            Console.WriteLine($"Libros mayores a S/10.99:");
-//            foreach (var precio in precios)
-//            {
-//                Console.WriteLine($"{precio.Nombre}, S/{precio.Precio}");
-//            }
-//            Console.WriteLine("");
+            // Encontrar el indice del libro que empiece con "El"
+            int indiceMLetra = libros.FindIndex(p => p.Nombre.StartsWith("El"));
+            Console.WriteLine($"Índice del libro que comienza en 'El' es: {indiceMLetra}");
+            Console.WriteLine("");
+            }
+       
+           class Libro
+            {
+                public string Nombre { get; set; }
+                public double Precio { get; set; }
+                public int Stock { get; set; }
+             }
 
-//            // Encontrar el indice del libro que empiece con "El"
-//            int indiceMLetra = libros.FindIndex(p => p.Nombre.StartsWith("El"));
-//            Console.WriteLine($"Índice del libro que comienza en 'El' es: {indiceMLetra}");
-//            Console.WriteLine("");
-//        }
-//    }
+      
 
+        
 
-//    // Clase auxiliar para demostraciones
-//    class Persona
-//{    public string Nombre { get; set; }
-//    public int Edad { get; set; }
-//    public string Dni { get; set; }
-//}
-
-//class Libro
-//    {
-//        public string Nombre { get; set; }
-//        public double Precio { get; set; }
-//        public int Stock { get; set; }
-//    }
-//    }
-  }
+    }
 }
